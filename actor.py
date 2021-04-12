f8c02a66601270fb8972bef48437a3c5cced8d31
Dylan Johnson <45766928+maxbear123@users.noreply.github.com>
2021-04-12 16:39:57 -0400

    Add files via upload

---
+++ actor.py
@@ -0,0 +1,100 @@
+import pygame
+import numpy as np
+import random
+import math as m
+
+
+# generates initial positions around the screen clockwise
+def actor_positions(n, res=(1920, 1080)):
+    perimeter = 2 * res[0] + 2 * res[1]
+    separation = perimeter / n
+    pos = []
+    for i in range(n):
+        if i * separation < res[0]:
+            pos.append((i * separation, 0))
+        elif i * separation - res[0] < res[1]:
+            pos.append((res[0], i * separation - res[0]))
+        elif i * separation - res[0] - res[1] < res[0]:
+            pos.append((i * separation - res[0] - res[1], res[1]))
+        elif i * separation - 2 * res[0] - res[1] < res[0]:
+            pos.append((0, i * separation - 2 * res[0] - res[1]))
+    return pos
+
+
+def actor_directions(positions, res=(1920, 1080)):
+    d = []
+    for i in positions:
+        if i[1] == 0:
+            theta = random.random() * m.pi
+        if i[0] == res[0]:
+            theta = random.random() * 1.5 * m.pi + m.pi * 0.5
+        if i[1] == res[1]:
+            theta = random.random() * m.pi * -1
+        if i[0] == 0:
+            theta = random.random() * 1.5 * m.pi + m.pi * 0.5 * -1
+        d.append((m.cos(theta), m.sin(theta)))
+    return d
+
+
+class Actor:
+    h = 1
+    G = 10
+
+    def __init__(self, screen, x, d, s=1, chars={'m': 1, 'r': 10}, res=(1920, 1080), rgb=(14, 242, 208)):
+        self.x = x
+        self.res = res
+        self.v = np.array(d) * s * 0
+        self.vtmp = self.v
+        self.chars = chars
+        self.a = np.array([0.0, 0.0])
+        self.r, self.g, self.b = rgb
+        self.screen = screen
+        pygame.draw.circle(self.screen, (self.r, self.g, self.b), (self.x[0], self.x[1]), self.chars['r'])
+
+    def calc_next(self, d_actors, actors):
+        if np.all(self.vtmp == self.v):
+            i2 = 0
+            for i in d_actors:
+                # This assumes that no two balls will make it to the same position
+                if i == 0:
+                    pass
+                else:
+                    m1, m2 = self.chars['m'], actors[i2].chars['m']
+                    v1, v2, x1, x2 = self.v, actors[i2].v, self.x, actors[i2].x
+                    if i <= (actors[i2].chars['r'] + self.chars['r']) and np.dot(actors[i2].x - self.x,
+                                                                                 self.v - actors[i2].v) > 0:
+                        # calculating collision
+                        self.vtmp = v1 - np.dot(v1 - v2, x1 - x2) * np.linalg.norm(x1 - x2) ** -2 * (
+                                x1 - x2) * 2 * m2 / (
+                                            m1 + m2)
+                        actors[i2].vtmp = v2 - np.dot(v2 - v1, x2 - x1) * np.linalg.norm(x2 - x1) ** -2 * (
+                                x2 - x1) * 2 * m1 / (
+                                                  m1 + m2)
+                    # calculating acceleration
+                    r_diff = x2 - x1
+                    self.a += r_diff * Actor.G * m2 / (i ** 3.0)
+                i2 += 1
+
+        if self.x[0] < 0:
+            self.x[0] = 0
+        if self.x[1] < 0:
+            self.x[1] = 0
+        if self.x[0] > self.res[0]:
+            self.x[0] = self.res[0]
+        if self.x[1] > self.res[1]:
+            self.x[1] = self.res[1]
+        if self.x[1] == 0:
+            self.vtmp[1] *= -1
+        if self.x[0] == self.res[0]:
+            self.vtmp[0] *= -1
+        if self.x[1] == self.res[1]:
+            self.vtmp[1] *= -1
+        if self.x[0] == 0:
+            self.vtmp[0] *= -1
+
+    def update(self):
+        self.v = self.vtmp
+        self.v += self.a * Actor.h
+        self.x += (self.v + self.vtmp) * 0.5 * Actor.h
+        self.a = np.array([0.0, 0.0])
+        pygame.draw.circle(self.screen, (self.r, self.g, self.b), (self.x[0], self.x[1]), self.chars['r'])
---
+++ evolve.py
@@ -0,0 +1,86 @@
+import sys
+import os
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import numpy as np
+import actor as act
+import pygame
+import random
+
+# controls:
+# w/s (while mousing over circle): increase/decrease circle radius
+# a/d (while mousing over circle): decrease/increase circle speed
+# up arrow/down arrow: increase/decrease all circle radii
+# left arrow/right arrow: decrease/increase all circle speeds
+
+act.h = 2
+n = 20
+screen_width, screen_height = 1440, 900
+res = (screen_width, screen_height)
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+done = False
+clock = pygame.time.Clock()
+msElapsed = clock.tick(30)
+positions = act.actor_positions(n, res)
+rand = [random.random() * 5 for i in range(n)]
+rand_s = [random.random() * 5 ** 0.5 for i in range(n)]
+actors = [
+    act.Actor(screen, np.array(positions[i]), act.actor_directions(positions, res)[i], s=1 + rand_s[i],
+              chars={'r': 10 + rand[i] ** 0.5, 'm': 1 + rand[i]},
+              res=res) for i in range(n)]
+while not done:
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_RIGHT]:
+        for i in actors: i.v *= 1.05
+    if pressed[pygame.K_LEFT]:
+        for i in actors: i.v /= 1.05
+    if pressed[pygame.K_q]:
+        act.Actor.h /= 1.05
+    if pressed[pygame.K_e]:
+        act.Actor.h *= 1.05
+    if pressed[pygame.K_w]:
+        for i in actors:
+            if np.linalg.norm(i.x - np.array(pygame.mouse.get_pos())) < i.chars['r']:
+                i.chars['m'] *= 1.05
+                i.chars['r'] *= 1.05 ** 0.5
+    if pressed[pygame.K_s]:
+        for i in actors:
+            if np.linalg.norm(i.x - np.array(pygame.mouse.get_pos())) < i.chars['r']:
+                i.chars['m'] /= 1.05
+                i.chars['r'] /= 1.05 ** 0.5
+    if pressed[pygame.K_a]:
+        for i in actors:
+            if np.linalg.norm(i.x - np.array(pygame.mouse.get_pos())) < i.chars['r']:
+                i.v /= 1.05
+    if pressed[pygame.K_d]:
+        for i in actors:
+            if np.linalg.norm(i.x - np.array(pygame.mouse.get_pos())) < i.chars['r']:
+                i.v *= 1.05
+    if pressed[pygame.K_UP]:
+        for i in actors:
+            i.chars['m'] *= 1.05
+            i.chars['r'] *= 1.05 ** 0.5
+    if pressed[pygame.K_DOWN]:
+        for i in actors:
+            i.chars['m'] /= 1.05
+            i.chars['r'] /= 1.05 ** 0.5
+    screen.fill((10, 10, 10))
+    for i in actors: i.update()
+    a = np.array([i.x for i in actors])
+    d_actors = np.linalg.norm(a - a[:, None], axis=-1)
+    print(d_actors)
+    E = 0
+    for i in range(len(actors)):
+        actors[i].calc_next(d_actors[i], actors)
+        for j in range(i + 1, len(actors), 1):
+            E -= act.Actor.G / d_actors[i][j] * actors[i].chars['m'] * actors[j].chars['m']
+    for i in actors:
+        E+=0.5*i.v[0]**2+i.v[1]**2*i.chars['m']
+    print(E)
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+            sys.exit()
